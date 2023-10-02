236e8d1f45c00e7a4c7204d5ac4ad53aec7d15b6
Yuliia-1102 <132250808+Yuliia-1102@users.noreply.github.com>
2023-10-02 06:10:20 +0300

    First commit

---
+++ Geography/Program.cs
@@ -0,0 +1,31 @@
+﻿abstract class GeographicObject
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public abstract string GetInformation();
+}
+
+class River : GeographicObject
+{
+    public double FlowSpeedCmPerSec { get; set; }
+    public double TotalLength { get; set; }
+
+    public override string GetInformation()
+    {
+        return $"Назва: {Name}\nКоординати: ({X}, {Y})\nОпис: {Description}\nШвидкість течії: {FlowSpeedCmPerSec} см/с\nЗагальна довжина: {TotalLength} км";
+    }
+}
+
+class Mountain : GeographicObject
+{
+    public double HighestPoint { get; set; }
+
+    public override string GetInformation()
+    {
+        return $"Назва: {Name}\nКоординати: ({X}, {Y})\nОпис: {Description}\n Найвища точка: {HighestPoint} метрів";
+    }
+}
+
---
+++ Interface/Program.cs
@@ -0,0 +1,33 @@
+﻿public interface IGeographicObject
+{
+    string GetInformation();
+}
+
+class River : IGeographicObject
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double FlowSpeedCmPerSec { get; set; }
+    public double TotalLength { get; set; }
+
+    public string GetInformation()
+    {
+        return $"Назва: {Name}\nКоординати: ({X}, {Y})\nОпис: {Description}\nШвидкість течії: {FlowSpeedCmPerSec} см/с\nЗагальна довжина: {TotalLength} км";
+    }
+}
+
+class Mountain : IGeographicObject
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double HighestPoint { get; set; }
+
+    public string GetInformation()
+    {
+        return $"Назва: {Name}\nКоординати: ({X}, {Y})\nОпис: {Description}\nНайвища точка: {HighestPoint} метрів";
+    }
+}
